b9b383157791a274fa30c62e2f164ae923476d18
detlux <95049640+detlux@users.noreply.github.com>
2024-03-21 16:51:05 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,7 @@
+with open('vacancy.csv', encoding='utf-8') as line:
+    arr = list(map(lambda x: x.strip().split(';'), line.readlines()))
+    sort = sorted(arr, key=lambda x: x[2])
+    for s in arr:
+        if s[3] == 'классный руководитель':
+            print(f"В компании {s[4]} есть заданная профессия: {s[3]}, з/п в такой компании составит: {s[0]}")
+            
