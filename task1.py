dac1fc674b7f3d198736e7ad9397e9d8fd06d354
detlux <95049640+detlux@users.noreply.github.com>
2024-03-21 17:53:42 +0300

    task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,13 @@
 with open('vacancy.csv', encoding='utf-8') as line, open('vacancy_new.csv', 'w', encoding='utf-8') as lene:
+    # обрабатываем исходный файл
     arr = list(map(lambda x: x.strip().split(';'), line.readlines()))[1:]
     arr.sort(key=lambda p: p[0], reverse=True)
+    # сортируем для нахрждения наибольшей з/п
     lene.write('company,role,Salary' + '\n')
+    # пишем первую строку 
     for j in arr:
         lene.write(f"{j[4]},{j[3]},{j[0]}" + '\n')
+    # создаем csv файл, как и требуется
     for i in range(3):
         print(arr[i][4], arr[i][3], arr[i][0], sep=' - ')
+    # выводим 3 наибольших
