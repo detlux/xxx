213ff6598b612ccbd15e7f1955a19553e4c63823
detlux <95049640+detlux@users.noreply.github.com>
2024-03-21 17:54:19 +0300

    task2.py

--- task2.py
+++ task2.py
@@ -1,7 +1,9 @@
 with open('vacancy.csv', encoding='utf-8') as line:
     arr = list(map(lambda x: x.strip().split(';'), line.readlines()))
     sort = sorted(arr, key=lambda x: x[2])
+    # сортируем
     for s in arr:
         if s[3] == 'классный руководитель':
             print(f"В компании {s[4]} есть заданная профессия: {s[3]}, з/п в такой компании составит: {s[0]}")
+            # находим подходящие
             
